746472fdbef1a6fad6b22e3b325be2c970afee4a
Cha1se <palamar.3003@mail.ru>
2024-01-17 12:09:39 +0700

    3 solution+

--- src/main/kotlin/easy/_3_Roman_to_Integer.kt
+++ src/main/kotlin/easy/_3_Roman_to_Integer.kt
@@ -2,38 +2,22 @@ package org.example.easy
 
 class _3_Roman_to_Integer {
     fun romanToInt(s: String): Int {
-        var res = 0
-        var isStreak = false
-        for(ch in 0..s.lastIndex step 2) {
-            var current = 0
-            var next = 0
-            if (ch == s.length) {
-                res += Roman.valueOf(s[ch].toString()).value
-                break
-            } else {
-                next = Roman.valueOf(s[ch+1].toString()).value
-                current = Roman.valueOf(s[ch].toString()).value
-            }
+        var result = 0
 
-            if (current == next) {
-                if (!isStreak) {
-                    res += (next + current)
-                    isStreak = true
+        for (i in s.indices) {
+            val currValue = Roman.valueOf(s[i].toString()).value
+            if (i + 1 != s.length) {
+                val nextValue = Roman.valueOf(s[i+1].toString()).value
+                if ( currValue < nextValue) {
+                    result -= currValue
                 } else {
-                    res += current
-                    isStreak = false
+                    result += currValue
                 }
-            } else if (current >= next) {
-                res += (current + next)
-                isStreak = false
             } else {
-                res += (next - current)
-                isStreak = false
+                result += currValue
             }
-
         }
-
-        return res
+        return result
     }
 }
 
